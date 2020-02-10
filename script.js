b2b020110972b4d1ccc382dfeef40cde617b18ee
Nick Reyno <nickrenoe@gmail.com>
2020-02-09 20:12:35 -0500

    safety commit

--- script.js
+++ script.js
@@ -4,20 +4,42 @@ app.p1 = [];
 app.p2 = [];
 app.p1Hand = [];
 app.p2Hand = [];
-app.p1Populate = [];
-app.p2Populate = [];
 app.cardOne = '';
 app.cardTwo = '';
 app.hitCard = [];
 app.p1Total = 0;
 app.p2Total = 0;
+app.p1Clone = [];
+app.p2Clone = [];
+
+
 
 // p1Hand is recieving array
 // p1 is the array for total calc
 //p1Populate is array for card represent
 
+// making a clone of initial unpopulated player areas and cards
+app.captureInitialState = function(){
+    for(let i=0, y=1; i<5; i++, y++){
+    app.p1Clone[i] = $(`.p1 .card${y}`).clone();
+    app.p2Clone[i] = $(`.p2 .card${y}`).clone();
+    }
+}
+
+// on next round this gets rid of old html and restores initial state
+app.restoreInitialState = function(){
+    for(let i=0, y=1; i<5; i++, y++){
+        $(`.p1 .card${y}`).replaceWith(app.p1Clone[i]);
+        $(`.p2 .card${y}`).replaceWith(app.p2Clone[i]);
+
+        // recloning the empty player areas so I can rinse and repeate
+        app.p1Clone[i] = $(`.p1 .card${y}`).clone();
+        app.p2Clone[i] = $(`.p2 .card${y}`).clone();
+    }
+}
 
 app.createDeck = () => {
+    app.cards.length = 0;
     for(let i = 2; i<15; i++) {
         // app.cards.push(i+" clubs");
         // app.cards.push(i+" diamonds");
@@ -51,6 +73,20 @@ app.dealCards = () => {
     }
 }
 
+app.hideMe = () => {
+$('.deal').removeClass('animatingIn').addClass('animatingOut')
+    .attr('disabled','true')
+    .css({
+        'animation-play-state':'running',
+        'animation-fill-mode':'forwards',
+        'animation-iteration-count': '1' });
+    };
+
+app.showMe = () => {
+    $('.deal').toggleClass('animatingOut, animatingIn')
+    .css('animation-fill-mode','reverse')
+    .removeAttr('disabled')
+}
 
 app.checkTotal = (player, playerHand, playerTotal) => {
     // parsing card number from raw data by splitting and taking the first value (card format ex [5, clubs])
@@ -66,12 +102,23 @@ app.checkTotal = (player, playerHand, playerTotal) => {
     // parse will ignore the words and only keep the number 🤯🤟🏻
     app[playerTotal] = (parseInt(app[player][0]) + parseInt(app[player][1]));
 
-    //check for blackjack
-    if (app.p1Total === 21) {
-        app.winBy21("p1");
-    } else if (app.p2Total == 21) {
-        app.loseSpecial();
+
+    // if dealt two aces
+        for (let i=0;i<app[player].length;i++){
+        if (app[playerTotal] > 21 && app[player][i][0] == 11 ) {
+                app[player][i][0] = 1;
+                app[playerTotal] -= 10;
+        }
+        $(`.${player}Score p`).text(app[playerTotal]);
     }
+
+    $(`.${player}Score p`).text(app[playerTotal]);
+    // //check for blackjack
+    // if (app.p1Total === 21) {
+    //     app.winBy21("p1");
+    // } else if (app.p2Total == 21) {
+    //     app.loseSpecial();
+    // }
 }
 
 // put data on empty divs to look like playing cards
@@ -109,7 +156,13 @@ app.populateCards = function(playerHand, player) {
     // color the diamond and heart cards
     app.colorCards("p1");
     app.colorCards("p2");
-    $(`.p2 .card1 h3, .p2 .card1 .cardSuit`).css('opacity','0');
+    $(`.p2 .card1 h3, .p2 .card1 .cardSuit, .p2 .card1 h4`).css('opacity','0');
+    if (app.p1Total == 21 || app.p2Total == 21) {
+        app.showHouse();
+        setTimeout(function(){
+            app.endRound()
+        }, 500)
+    }
 }
 
 app.colorCards = function(player){
@@ -123,159 +176,201 @@ app.colorCards = function(player){
 
 // logic to hit new cards
 app.hit = function(player, playerHand, playerTotal){
-    // pick a random card
-    app.hitCard = Math.floor(Math.random() * app.cards.length);
-    // find card value & push card to hand
-    app[playerHand].push(app.cards[app.hitCard]);
-    // take card out of the deck
-    app.cards.splice(app.hitCard, 1);
+    setTimeout(function(){
+        // pick a random card
+        app.hitCard = Math.floor(Math.random() * app.cards.length);
+        // find card value & push card to hand
+        app[playerHand].push(app.cards[app.hitCard]);
+        // take card out of the deck
+        app.cards.splice(app.hitCard, 1);
 
 
-    // pushing card to the player array to do face card logic 
-    let i = (app[playerHand].length - 1);
-    app[player].push(app[playerHand][i].split(" "));
+        // pushing card to the player array to do face card logic 
+        let i = (app[playerHand].length - 1);
+        app[player].push(app[playerHand][i].split(" "));
 
-    // face card logic for array value
-    if (parseInt(app[player][i][0]) > 11) {
-        app[player][i][0] = 10;
-    }
-
-        // face card display in hand logic
-    for (let b=0;b<app[player].length;b++){
-        app[playerHand][i].split(" ");
-        if (parseInt(app[playerHand][b]) == 11 || app[playerHand][i] == 1) {
-            app[playerHand][b] = "A"
+        // face card logic for array value
+        if (parseInt(app[player][i][0]) > 11) {
+            app[player][i][0] = 10;
         }
-        if(parseInt(app[playerHand][b]) == 12) {
-            app[playerHand][b] = "J"
-        } if (parseInt(app[playerHand][b]) == 13) {
-            app[playerHand][b] = "Q"
-        } if (parseInt(app[playerHand][b]) == 14) {
-            app[playerHand][b] = "K"
+
+            // face card display in hand logic
+        for (let b=0;b<app[player].length;b++){
+            app[playerHand][i].split(" ");
+            if (parseInt(app[playerHand][b]) == 11 || app[playerHand][i] == 1) {
+                app[playerHand][b] = "A"
+            }
+            if(parseInt(app[playerHand][b]) == 12) {
+                app[playerHand][b] = "J"
+            } if (parseInt(app[playerHand][b]) == 13) {
+                app[playerHand][b] = "Q"
+            } if (parseInt(app[playerHand][b]) == 14) {
+                app[playerHand][b] = "K"
+            }
         }
-    }
 
-    // populateNewCard
-    let q = (app[playerHand].length)
-    // show card
-    $(`.${player} .card${q}`).css('display','flex')
-    // populate card
-    $(`.${player} .card${q} h3, .${player} .card${q} h4`)
-    .text(app[playerHand][i].split(" ")[0])
-    .addClass(app[player][i][1]);
+        // populateNewCard
+        let q = (app[playerHand].length)
+        // show card
+        $(`.${player} .card${q}`).css('display','flex').addClass('animated');
+        // populate card
+        $(`.${player} .card${q} h3, .${player} .card${q} h4`)
+        .text(app[playerHand][i].split(" ")[0])
+        .addClass(app[player][i][1]);
 
-    // populate middle suit symbol
-    $(`.${player} .card${q} .cardSuit`).text(app[player][i][1]);
+        // populate middle suit symbol
+        $(`.${player} .card${q} .cardSuit`).text(app[player][i][1]);
 
-    // color those diamonds and hearts
-    app.colorCards(player)
-    
-    // get new total
-    app[playerTotal] += parseInt(app[player][i]);
-    console.log(app[playerTotal])
+        // color those diamonds and hearts
+        app.colorCards(player)
+        
+        // get new total
+        app[playerTotal] += parseInt(app[player][i]);
 
-    // check for bust
-    for (let i=0;i<app[player].length;i++){
-        // ace logic
-        if (app[playerTotal] > 21 && app[player][i][0] == 11 ) {
-                app[playerTotal] -= 10;
+        // check for bust
+        for (let i=0;i<app[player].length;i++){
+            // ace logic
+            if (app[playerTotal] > 21 && app[player][i][0] == 11 ) {
+                    app[player][i][0] = 1;
+                    app[playerTotal] -= 10;
+            }
+            $(`.${player}Score p`).text(app[playerTotal]);
         }
-    }
-    if (app.p1Total > 21) {
-        app.lose();
-    }
-    app.checkWin();
+        if (app.p1Total > 21) {
+            app.lose();
+        } else {
+            app.checkWin();
+        }
+    },200)
 }
+
 
 app.checkWin = function(){
     if (app.p1Total == 21) {
         app.winBy21();
     } else if (app.p2Total == 21) {
-        app.lose();
+        app.loseSpecial();
     }
 }
 
 app.showHouse = function(){
-    $(`.p2 .card1 h3, .p2 .card1 .cardSuit`).css('opacity','1');
-    $(`.p2 .card1`).css('background','#FAFAFA');
+    $(`.p2 .card1`).css('background','#FAFAFA').addClass('animated');
+    $(`.p2 .card1 h3, .p2 .card1 .cardSuit, .p2 .card1 h4`).css('opacity','1');
+    $(`.p2Score p`).css('opacity','1');
 
 }
 
 app.computerHit = function() {
-    if (app.p2Total < 17 && app.p1Total > app.p2Total ) {
+    setTimeout(function(){
+        if (app.p2Total < 17 && app.p1Total > app.p2Total ) {
+            app.hit("p2", "p2Hand", "p2Total");
+        }
+    },200);
+    setTimeout(function(){
+        if  (app.p2Total < 17 && app.p1Total > app.p2Total ) {
+            app.hit("p2", "p2Hand", "p2Total");
+        }  
+    },600);
+    setTimeout(function(){
+            if  (app.p2Total < 17 && app.p1Total > app.p2Total ) {
         app.hit("p2", "p2Hand", "p2Total");
-    }
-    if  (app.p2Total < 17 && app.p1Total > app.p2Total ) {
-        app.hit("p2", "p2Hand", "p2Total");
-    }  
-    if  (app.p2Total < 17 && app.p1Total > app.p2Total ) {
-        app.hit("p2", "p2Hand", "p2Total");
-    }  
-    app.endRound();
+        }  
+    },1000)
+    setTimeout(function(){
+
+        app.endRound();
+    },1000);
     
 }
 
 app.endRound = function(){
-    if (app.p2Total > 21) {
+    if (app.p1Total === 21) {
+        app.winBy21();
+    } else if( app.p2Total === 21) {
+        app.loseSpecial();
+    } else if (app.p2Total > 21) {
         app.winEnd();
-    }else if (app.p1Total > app.p2Total) {
+    } else if (app.p1Total > app.p2Total) {
         app.winEnd();
-    } else if (app.p1Total == app.p2Total){
-        app.winEnd();
-    }
-    else if (app.p1Total < app.p2Total && app.p2Total < 22) {
+    } else if (app.p1Total === app.p2Total){
+        app.winTie();
+    } else {
         app.lose();
     }
-    console.log("p1 " + app.p1Total)
-    console.log("p2 " + app.p2Total)
+}
+
+app.nextRound = function(){
+    $('.winScreen').css('display','none')
+    if (app.cards.length < 16) {
+        app.createDeck();
+    }
+    app.p1.length = 0;
+    app.p2.length = 0;
+    app.p1Hand.length = 0;
+    app.p2Hand.length = 0;
+    app.p1Total = 0;
+    app.p2Total = 0;
 }
 
 // ===================== //
 //  win state  functions //
 // ===================== //
 $win = $(`.winScreen`);
+
 app.winScreen = function(){
-$win.css('display','block')
+$win.css('display','block');
+$win.children('h2').text('Winner! Ganier!');
+
 }
 
-app.winBy21 = function(player) {
+app.winBy21 = function() {
     app.winScreen();
-    $win.children('p').text('YOU DID IT HELL YEAH')
+    $win.children('h2').text(`BLACKJACK!!`);
+    $win.children('p').text(`Congrats! You get the big bucks$$$`);
 }
 
 app.winEnd = function(){
     app.winScreen();
-    $win.children('p').text('YOU DID IT HELL YEAH')
+    $win.children('p').text('YOU DID IT HELL YEAH');
+}
 
+app.winTie = function(){
+    app.winScreen();
+    $win.children('p').text(`lucky for you, ties don't go to the house!`);
 }
 
 app.lose = function(){
     app.winScreen();
-    $win.children('h2').text('wasted')
-    $win.children('p').text('you fuckin lost bro')
+    $win.children('h2').text('wasted');
+    $win.children('p').text('you fuckin lost bro');
 }
 
 app.loseSpecial = function(){
     app.winScreen();
-    $win.children('h2').text('wasted')
-    $win.children('p').text(`house got 21, that's just bad luck`)
+    $win.children('h2').text('wasted');
+    $win.children('p').text(`house got 21, that's just bad luck`);
 }
 
 
-
-
-
-
 app.init = function(){
+
+    // was having weird resizing issues with vh units on firefox
+    $(window).on('resize',function(){
+        let heightFix = $(window).height(); 
+        $('.wrapper').css('height', `${heightFix}`)
+        });
+
+    app.captureInitialState();
     app.createDeck();
 
-    $('.rulesButton').on('click',function(){
+    $('.rulesButton, .letsPlay, .closeRules').on('click',function(){
         $('.rulesModal').toggleClass('show, hide');
     })
 
     $(`.deal`).on('click',function(e){
     e.preventDefault();
     app.dealCards();
+    app.hideMe();
     app.checkTotal("p1", "p1Hand", "p1Total");
     app.checkTotal("p2", "p2Hand", "p2Total");
     app.populateCards("p1Hand", "p1");
@@ -292,7 +387,19 @@ app.init = function(){
     e.preventDefault();
     app.showHouse();
     app.computerHit();
+    });
     // disable the hit button once player has chosen to stay, remember to re-enable this on new round
+    
+    $('.playAgain').on('click',function(e){
+        e.preventDefault();
+        app.nextRound();
+        app.showMe();
+        app.restoreInitialState();
+    });
+
+    $('.dark').on('click', () => {
+        $(':root').css({
+        "--primary": "red"});
     });
 };
 
